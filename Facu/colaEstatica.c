27669b8800048925a1f6bcba580e254220bdbbf2
Sebastian <sebastian_litter@hotmail.com>
2015-05-15 16:21:41 -0300

    colaEstatica completa

--- Facu/colaEstatica.c
+++ Facu/colaEstatica.c
@@ -1,4 +1,45 @@
-﻿void vaciarCola(t_cola *p)
+﻿#include <stdio.h>
+#include <stdlib.h>
+#define TAM 5
+
+typedef struct{
+	ind dato;
+}t_info;
+
+typedef struct{
+	t_info cola[TAM];
+	int pri;
+	int ult;
+}t_cola;
+
+void crearCola(t_cola *);
+int colaVacia(const t_cola *);
+int colaLlena(const t_cola *);
+void vaciarCola(t_cola *);
+int cargarInfo(t_info *);
+int ponerEnCola(t_cola *, const t_info *);
+int sacarDeCola (t_cola * , const t_info *);
+int verPrimero(const t_cola *, t_info *);
+void mostrarInfo(t_info *);
+
+
+void crearCola(t_cola *p)
+{
+	p->pri=0;
+	p->ult=-1;
+}
+
+int colaVacia(const t_cola *p)
+{
+	return p->ult==-1;
+}
+
+int colaLlena(const t_cola *p)
+{
+	return p->pri==0 && p->ult==TAM-1 || p->ult+1==p->pri && p->ult!=-1;
+}
+
+void vaciarCola(t_cola *p)
 {
 	p->pri=0;
 	p->ult=-1;
@@ -48,4 +89,30 @@ int cargarInfo(t_info *d)
 void mostrarInfo(t_info *d)
 {
 	printf("%d",d);
+}
+
+
+int main()
+{
+	t_info info;
+	t_cola cola;
+	crearCola(&cola);
+	if(!colaVacia(&cola))
+	{
+		vaciarCola(&cola);
+	}
+	if(!colaLlena(&cola))
+	{
+		cargarInfo(&info);
+		if(ponerEnCola(&cola, &info))
+			puts("Puso OK");
+	}
+	if(!colaVacia(&cola))
+	{
+		if(sacarDeCola(&cola, &info))
+			puts("Saque de cola");
+		if(verPrimero(&cola,&info))
+			mostrarInfo(&info);	
+	}
+	return 0;
 }
